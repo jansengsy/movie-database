d5e89eb06b9459b982abc0f484bc85e9229aaeeb
kees <keessqq@git.training>
2022-05-30 20:42:13 +0100

    Table now has data from get all route passed in as bound props

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="content w-10/12">
-    <MovieTable />
+  <div class="content w-10/12 border-2 border-solid border-gray-500 mb-2 mt-2 p-2">
     <MovieForm />
+    <MovieTable :movies="movies"/>
   </div>
 </template>
 
@@ -9,10 +9,35 @@
   import MovieTable from './components/MovieTable';
   import MovieForm from "./components/MovieForm";
 
+  import axios from 'axios';
+
   export default {
+    data() {
+      return {
+        movies: [],
+        errors: {},
+      }
+    },
     components: {
       MovieTable,
       MovieForm,
     },
+    created() {
+      this.getMovies();
+    },
+    methods: {
+      async getMovies() {
+        try {
+          const response = await axios.get('http://localhost:8000/api/movies');
+          this.movies = response.data;
+        } catch (error) {
+          if (error.response.status === 422) {
+            this.errors = error.response.data.errors;
+          } else {
+            console.log(error);
+          }
+        }
+      }
+    }
   }
 </script>
--- resources/js/components/MovieTable.vue
+++ resources/js/components/MovieTable.vue
@@ -1,3 +1,38 @@
 <template>
-  <h1>Movie table</h1>
+  <table class="table-fixed min-w-full">
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Year</th>
+          <th>Director</th>
+          <th>Description</th>
+        </tr>
+      </thead>
+      <tbody>
+          <tr class="border-b" v-for="movie in movies" :key="movie.id">
+            <td>{{ movie.title }}</td>
+            <td>{{ movie.year }}</td>
+            <td>{{ movie.director }}</td>
+            <td>{{ movie.description }}</td>
+            <td>
+              <div role="group">
+                <button>Edit</button>
+              </div>
+            </td>
+            <td>
+              <div role="group">
+                <button>Delete</button>
+              </div>
+            </td>
+          </tr>
+      </tbody>
+    </table>
 </template>
+
+<script>
+  export default {
+    props: [
+      'movies',
+    ]
+  }
+</script>
